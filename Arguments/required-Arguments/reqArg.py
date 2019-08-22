2a7f64537c914f9c4401df7f106e329b6e77b4ae
Ankit Mittal <ankit630@gmail.com>
2019-08-22 17:25:24 +0000

    Added the required argument

---
+++ Arguments/required-Arguments/reqArg.py
@@ -0,0 +1,8 @@
+def reqArgFunc(empname):
+    print("Emp name: ", empname)
+    return;
+print("Not passing required arg value")
+#reqArgFunc()
+print("Now passing required arg value")
+reqArgFunc("Hello")
+
